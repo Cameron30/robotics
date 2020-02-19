0b0fabfb9e76fac5bdd3b7c4c337efa94303c5e6
Cameron30 <cameronray@cox.net>
2020-02-19 17:15:51 -0600

    added the proj1 folder
    Using my instructions, I got the turtlebot up and running in the right map, with collision on the walls, and it is drivable. The demo location_monitor_node.py is added to the launch file, but it has an error.

---
+++ proj1/src/location_monitor_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import Odometry
+
+def callback(msg):
+	x = msg.pose.pose.position.x
+	y = msg.pose.pose.position.y
+
+def main():
+	rospy.init_node('location_monitor')
+	rospy.Subcriber("/odom", Odometry, callback)
+	rospy.spin()
+
+if__name__ == '__main__':
+	main()
