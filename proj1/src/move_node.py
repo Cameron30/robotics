55b4536b235e80e8275afe0a39c97761b4e7374b
PhillipVoss <47005062+PhillipVoss@users.noreply.github.com>
2020-02-20 16:24:44 -0600

    added a node for moving the robot
    subcribes to the velocity cntrl and moves the robot

---
+++ proj1/src/move_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import time
+import rospy
+from geometry_msgs.msg import Twist
+
+def main():
+	rospy.init_node('move_node')
+
+	publisher = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+
+	while not rospy.is_shutdown():
+		msg = Twist()
+		msg.linear.x = .2 
+		publisher.publish(msg)
+
+if __name__ == '__main__':
+	main()
