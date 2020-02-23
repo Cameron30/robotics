b19b10d245e167729cd25d88cf57485ce6417373
PhillipVoss <47005062+PhillipVoss@users.noreply.github.com>
2020-02-23 17:46:18 -0600

    updated to now move and send messages from bumper when pressed

--- proj1/src/move_node.py
+++ proj1/src/move_node.py
@@ -3,16 +3,42 @@
 import time
 import rospy
 from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent
+
+def shutdown(self):
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        cmd_vel.publish(Twist())
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the 		# script
+        rospy.sleep(1)
+def processBump(data):
+	print ("Bump")
+	global bump
+	if (data.state == BumperEvent.PRESSED):
+		bump = True
+	else:
+		bump = False
+	rospy.loginfo("Bumper Event")
+	rospy.loginfo(data.bumper)
 
 def main():
 	rospy.init_node('move_node')
 
 	publisher = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size=10)
+	sub_bump = rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, processBump)
 
+	r = rospy.Rate(10)
+	
+	msg = Twist()
+	msg.linear.x = 0.2
 	while not rospy.is_shutdown():
-		msg = Twist()
-		#msg.linear.x = .2 
-		#publisher.publish(msg)
+		publisher.publish(msg)
+		r.sleep()
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except rospy.Service.Exception as exc:
+		rospy.loginfo("Location_Monitor_node terminated.")
+		print("Service did not process request: " + str(exc))
