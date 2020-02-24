de53f28c9740cf7c14f7f519c6348cc6f2a3a9de
Yilei Shen <48291718+YileiShen@users.noreply.github.com>
2020-02-24 13:24:50 -0600

    Add files via upload

--- proj1/src/test_movement.py
+++ proj1/src/test_movement.py
@@ -5,21 +5,34 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64
 
 def my_callback(msg):
+
+	global angle
+	global clockwise
+	
+	#converting from angles to radians
+	angle = 15
+	clockwise = [1, -1]
+	relative_angle = angle * 2 * PI / 360
 	
 	distance_moved = msg.data
-
-	if msg.data < 2:
+	
+	#1ft = 0.3048m
+	if msg.data < 0.3048:
 		move.linear.x = 0.1
 
-	if msg.data > 2:
+	if msg.data >= 0.3048:
+		msg.angular.z = relative_angle * random.choice(clockwise)
+		msg.data = 0
 		move.linear.x = 0
-
+		
 	pub.publish(move)
 
-rospy.init_node('test_movement')
+if __name__ == '__main__':
 
-sub = rospy.Subscriber('moved_distance', Float64, my_callback)
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size = '1')
-move = Twist()
+	rospy.init_node('test_movement')
 
-rospy.spin()
+	sub = rospy.Subscriber('moved_distance', Float64, my_callback)
+	pub = rospy.Publisher('/cmd_vel', Twist, queue_size = '1')
+	move = Twist()
+
+	rospy.spin()
